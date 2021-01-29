a5e68a879a505a4803f7290e320c3600c79def58
diegosalazar343 <diegosalazar343@gmail.com>
2021-01-29 15:57:17 -0800

    pizza size price is working but not other values

--- js/scripts.js
+++ js/scripts.js
@@ -7,38 +7,38 @@ function Pizza (size, meatToppings, veggieToppings) {
   this.size = size;
   this.meatToppings = meatToppings;
   this.veggieToppings = veggieToppings;
-  this.price = 0;
+  this.pizzaPrice = 4;
 }
 
 
 Pizza.prototype.cost = function() {
   if(this.size === "small") {
-    this.pizzaPrice += 6;
+    this.pizzaPrice += 2;
   } else if(this.size === "medium"){
-    this.pizzaPrice += 7;
-  } else if (this.size === "large") {
-    this.pizzaPrice += 8;
-  } else if (this.size === "extra-large"){
-    this.pizzaPrice += 9;
-  };
-  if(this.meatToppings === "2") {
-    this.pizzaPrice += 2;
-  } else if (this.meatToppings === "4") {
-    this.pizzaPrice += 4;
-  } else if (this.meatToppings === "5") {
-    this.pizzaPrice += 5;
-  } else if (this.meatToppings === "1") {
-    this.pizzaPrice += 1;
-  } else if (this.meatToppings === "7") {
-    this.pizzaPrice += 7;
-  }
-  if(this.veggieToppings === "1") {
-    this.pizzaPrice += 1;
-  } else if(this.veggieToppings === "2") {
-    this.pizzaPrice += 2;
-  } else if (this.veggieToppings === "3"){
     this.pizzaPrice += 3;
-  } else if (this.veggieToppings === "15") {
+  } else if (this.size === "large") {
+    this.pizzaPrice += 4;
+  } else if (this.size === "extra-large"){
+    this.pizzaPrice += 5;
+  };
+  if(this.meatToppings === 1) {
+    this.pizzaPrice += 1;
+  } else if (this.meatToppings === 2) {
+    this.pizzaPrice += 2;
+  } else if (this.meatToppings === 4) {
+    this.pizzaPrice += 4;
+  } else if (this.meatToppings=== 5) {
+    this.pizzaPrice += 5;
+  } else if (this.meatToppings === 7) {
+    this.pizzaPrice += 7;
+  };
+  if(this.veggieToppings === 1) {
+    this.pizzaPrice += 1;
+  } else if(this.veggieToppings === 2) {
+    this.pizzaPrice += 2;
+  } else if (this.veggieToppings === 3){
+    this.pizzaPrice += 3;
+  } else if (this.veggieToppings === 15) {
     this.pizzaPrice += 15;
   }
 };
@@ -50,12 +50,12 @@ $(document).ready(function() {
 
     let meat = $("input:checkbox[name=meat]:checked");
     let meatToppings = meat;
-    let veggie = $("input:checkbox[name=veggies]:checked");
-    let veggieToppings = veggie;
+    let veggies = $("input:checkbox[name=veggies]:checked");
+    let veggieToppings = veggies;
     const size = $("#size").val();
 
     newPizza = new Pizza(size, meatToppings, veggieToppings);
     newPizza.cost();
-    $("#order").text('Hey your order comes out to be $' + newPizza.price + '.00');
+    $("#order").text('Your order comes out to be $' + newPizza.pizzaPrice + '.00');
   });
 });
